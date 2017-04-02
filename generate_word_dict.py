abaa68ab968cbc446da3734105f19ee63f691cdb
Aldy Syahdeini <s1575408@rieti.inf.ed.ac.uk>
2017-04-02 21:08:20 +0100

    edit generate word_dict

--- generate_word_dict.py
+++ generate_word_dict.py
@@ -12,6 +12,8 @@ input_roots = "helper_data/roots.en.txt"
 output_suffix = "helper_data/suffixes_dict.p"
 output_roots = "helper_data/roots_dict.p"
 output_all = "helper_data/all_dict.p"
+melted_dict_filename = "helper_data/melted_dict.p"
+unmelted_word_filename = "helper_data/unmelted_dict.p"
 
 
 suffix_file_in = open(input_suffix,"r")
@@ -19,10 +21,21 @@ suffix_file_out = open(output_suffix,"wb")
 roots_file_in = open(input_roots,"r")
 roots_file_out = open(output_roots,"wb")
 output_all_out = open(output_all,"wb")
+melted_file_dict = open(melted_dict_filename,"wb")
+unmelted_file_dict = open(unmelted_word_filename,"wb")
+
 # Reading suffix file
+
+melted_dict = {}
+unmelted_list = []
 suffixes = set()
 for line in suffix_file_in:
     suff = line.strip().split(" ",1)[0].split("/")[0]
+    if len(suff)>1:
+        unmelted_list.append(suff)
+        melted_dict[suff[:-1]] = suff
+        suff = suff[:-1]
+
     suffixes.add(suff)
 pickle.dump(suffixes,suffix_file_out)
 
@@ -31,10 +44,17 @@ roots = set()
 for line in roots_file_in:
      root = line.strip().split(" ",1)[0].split("/")[0]
      if len(root)>1:
-     	root = root[:-1]
+        unmelted_list.append(root)
+        melted_dict[root[:-1]] = root
+        root = root[:-1]
      roots.add(root)
 pickle.dump(roots,roots_file_out)
 all_set = suffixes | roots
 pickle.dump(all_set,output_all_out)
 output_all_out.close()
 
+pickle.dump(melted_dict,melted_file_dict)
+pickle.dump(unmelted_list,unmelted_file_dict)
+
+melted_file_dict.close()
+unmelted_file_dict.close()
