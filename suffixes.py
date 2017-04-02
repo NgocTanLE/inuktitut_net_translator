e302a08949dc9820284c53f6e6689bb3877fd0a8
dinosaurioinvisible <ferodriguezv@gmail.com>
2017-04-02 02:12:24 +0100

    Add files via upload

--- suffixes.py
+++ suffixes.py
@@ -1,6 +1,7 @@
 
 # -*- coding: utf-8 -*-
 from collections import defaultdict
+import re
 #file = "allSuffixes.txt"
 
 def get_suffixes(file):
@@ -8,8 +9,11 @@ def get_suffixes(file):
     suffixes = defaultdict(dict)
     with open(file) as f:
         for line in f:
+            if re.search(r'\(...\)', line):
+                mline = line.split()
+                morpheme = mline[1]
             if "-->" in line:
                 sline = line.split()
-                #suffixes[sline[-3][1]].append([sline[-1], sline[1]])
-                suffixes[sline[-3][1]][sline[-1][1:]] = sline[1]
+                #suffixes[sline[-3][1]][sline[-1][1:-1]] = sline[1]
+                suffixes[sline[-3][1]][sline[-1][1:-1]] = morpheme
     return suffixes
